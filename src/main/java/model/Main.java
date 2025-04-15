167ef9d19dfab878e7c907f526e27b8ffc323629
TrialShock2629github <sanselmoluca3@gmail.com>
2025-04-15 12:00:07 +0200

    First Commit

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -2,6 +2,6 @@ package model;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Codice di prova");
+        System.out.println("Codice di test");
     }
 }
