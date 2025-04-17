74657ba3dba7baf064da2644e26b931e78a32c40
TrialShock2629github <sanselmoluca3@gmail.com>
2025-04-17 10:15:34 +0200

    Test commit n.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ import model.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Commit di prova");
+        System.out.println("Commit di prova n.2");
     }
 }
