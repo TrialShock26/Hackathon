f736b4f2922eb03de3b9dfaa4772a4e6e56672b1
TrialShock2629github <sanselmoluca3@gmail.com>
2025-05-20 11:53:55 +0200

    Version 1.5: test commit for refactoring

--- Object Orientation/src/main/java/Main.java
+++ Object Orientation/src/main/java/Main.java
@@ -2,6 +2,6 @@ import model.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Piattaforma di gestione di hackathon");
+        System.out.println("Piattaforma per la gestione di hackathon");
     }
 }
