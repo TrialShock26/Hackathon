986267d5ac3fe742a6daa7e2a98c35a3e7591c54
TrialShock2629github <sanselmoluca3@gmail.com>
2025-10-03 11:38:34 +0200

    Version 3.0.2: added FlatLaf dependancy

--- Object Orientation/src/main/java/Main.java
+++ Object Orientation/src/main/java/Main.java
@@ -1,7 +1,18 @@
 import gui.*; //TODO
 
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.FlatClientProperties;  //TODO
+
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            JOptionPane.showMessageDialog(null, "C'è stato un errore nella visualizzazione estetica", "Error", JOptionPane.ERROR_MESSAGE);
+        }
         new RegistrationGUI();
     }
 }
