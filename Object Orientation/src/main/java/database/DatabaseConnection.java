f8576cfee5c656c8047a623b8c7c3ff1f86469ed
TrialShock2629github <sanselmoluca3@gmail.com>
2025-10-06 11:30:40 +0200

    Version 3.0.3: added database package and class

---
+++ Object Orientation/src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,33 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    public Connection connection;
+
+    private DatabaseConnection() {
+        try {
+            String url = "jdbc:postgresql://ep-restless-rain-a2a23td0-pooler.eu-central-1.aws.neon.tech/neondb?user=neondb_owner&password=npg_uRIqoh0V6lnb&sslmode=require&channelBinding=require";
+            String user = "neondb_owner";
+            String password = "npg_uRIqoh0V6lnb";
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null)
+            instance = new DatabaseConnection();
+        else if (instance.connection.isClosed())
+            instance = new DatabaseConnection();
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return connection;
+    }
+}
